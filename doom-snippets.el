f54d640ba95fabf2d915b0edfc76e16964820865
werhner <gao9426@hotmail.com>
2024-06-27 17:28:55 +0800

    no-littnering

--- doom-snippets.el
+++ doom-snippets.el
@@ -50,7 +50,7 @@
   "Add `doom-snippets-dir' to `yas-snippet-dirs', replacing the default
 yasnippet directory."
   (setq yas-wrap-around-region nil)
-  (add-to-list 'yas-snippet-dirs 'doom-snippets-dir)
+  (add-to-list 'yas-snippet-dirs doom-snippets-dir)
   (yas-load-directory doom-snippets-dir t))
 
 ;;;###autoload
